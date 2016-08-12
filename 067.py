303f53d85225b0709d117ae451e7219e4bf64a34
roflmaostc <f.wechsler@tum.de>
2016-08-12 13:24:02 +0200

    067 solved

--- 067.py
+++ 067.py
@@ -15,10 +15,13 @@ Find the maximum total from top to bottom in triangle.txt (right click and 'Save
 NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
 """
 
-def nextNodes(x,y):
-        return [y+1,x],[y+1,x+1]
 
+
+#MY SOLUTION IDEA
+#I solve the problem from bottom to top. You start in the second last line and look which of the belows nodes is better. You add both numbers and do this procedure for alle the nodes in one line.
+#Continue with the next line and add again the previous some to the current node sum
 def loadTriangleToList(filename):
+    """Converts the triangle txt into a list"""
         #read file
     with open(filename) as file:
         data = file.readlines()
@@ -27,7 +30,34 @@ def loadTriangleToList(filename):
     list=[[int(i) for i in entry.split()] for entry in data]
     return list
 
+
+def bottomToTop(triangle):
+    nodeSum=triangle
+    i=98
+    while i>=0:
+        for j in range(i+1):
+            nodeSum[i][j]+=maxValueOfKids(nodeSum,i,j)
+        i-=1
+    return nodeSum[0][0]
+
+def maxValueOfKids(triangle, i,j):
+    left=triangle[i+1][j]
+    right=triangle[i+1][j+1]
+    if left>right:
+        return left
+    else:
+        return right
+
+#OLD PART
+
+def nextNodes(x,y):
+        return [y+1,x],[y+1,x+1]
+
+
 def valueAtNode(point,triangle):
+    """Returns the value of a node.
+       Point is a tuple of x and yy (y,x). Triangle is the list of
+    """
     return triangle[point[0]][point[1]]
 
 def dumbMethod(triangle):
@@ -48,7 +78,6 @@ def dumbMethod(triangle):
 
         
     return sum(way)
+#OLD PART END
 
-
-triangle=loadTriangleToList("triangle.txt")
-print(dumbMethod(triangle))
+print(bottomToTop(loadTriangleToList("triangle.txt")))
