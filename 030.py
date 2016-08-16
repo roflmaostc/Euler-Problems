442b0fdc0f639a259b9a5e86a73bc42047b0d271
roflmaostc <f.wechsler@tum.de>
2016-08-17 01:13:25 +0200

    solved. But not strict mathematical

---
+++ 030.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+
+
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+    1634 = 14 + 64 + 34 + 44
+    8208 = 84 + 24 + 04 + 84
+    9474 = 94 + 44 + 74 + 44
+
+As 1 = 14 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+"""
+
+
+def solveProblem(limit=1000000):
+    return sum(powerSum(i) for i in range(10,limit) if i==powerSum(i))
+
+
+def powerSum(number):
+    return sum([int(digit)**5 for digit in str(number)])
+
+
+print(solveProblem())
