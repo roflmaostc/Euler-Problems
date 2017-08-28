6b2be80687f088d5822bd03f3e9068b613e676c9
roflmaostc <felix.wechsler@tum.de>
2017-08-29 00:48:08 +0200

    modified

--- 037.py
+++ 037.py
@@ -13,11 +13,13 @@ def isPrime(number):
     import numpy as np
     if number==1:
         return False
-    if number==2:
+    if number==2 or number==3:
         return True
     if number%2==0:
         return False
-    for i in range(3, int(np.sqrt(number)//1)+1,2 ):
+    if number % 3 == 0:
+        return False
+    for i in range(3, int(np.sqrt(number)//1)+1,2):
         if number%i==0:
             return False
     return True
