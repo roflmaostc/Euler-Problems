a1690bd538d9b070eab50829d3d682dd32744e68
roflmaostc <f.wechsler@tum.de>
2017-08-30 01:02:21 +0200

    043 solved

--- eulerfunctions.py
+++ eulerfunctions.py
@@ -19,7 +19,6 @@ def isPrime(number):
     return True
 
 
-
 def isPandigital(number,n):
     s = str(number)
     for i in range(1,n+1):
@@ -31,14 +30,68 @@ def isPandigital(number,n):
     return True
 
 
-#from https://stackoverflow.com/a/2489519
 def is_square(apositiveint):
+    #from https://stackoverflow.com/a/2489519
+    x = apositiveint // 2
+    seen = set([x])
+    while x * x != apositiveint:
+        x = (x + (apositiveint // x)) // 2
+        if x in seen: return False
+        seen.add(x)
+    return True
 
-  x = apositiveint // 2
-  seen = set([x])
-  while x * x != apositiveint:
-    x = (x + (apositiveint // x)) // 2
-    if x in seen: return False
-    seen.add(x)
-  return True
+
+def is_pandigital(num, digits):
+    s = str(num)
+
+    for i in digits:
+        if str(i) not in s:
+            return False
+
+    return True
+
+
+
+def is_pandigital2(num, digitsA):
+    #digitsA is e.g. 54321
+    res = 0
+    while num>=1:
+        digit = num%10
+        num //= 10
+        res += 10**(digit-1)*digit 
+
+    return res == digitsA
+
+
+def rec_pandigital(base, numbers, generatedNumbers):
+    """
+    This function creates all permutations of a number of concatenated elements of parameter numbers
+    """
+    numbersForLoop=numbers.copy()
+    for digit in numbersForLoop: 
+        numbers.remove(digit)
+        base=appendDigit(base, digit)
+       
+        if len(numbers)>0:
+            rec_pandigital(base, numbers, generatedNumbers)
+        else:
+            generatedNumbers.append(base)
+        
+        base=removeDigit(base, digit)
+        numbers.append(digit)
+    return  
+
+def create_pandigitals(numbers):
+    allNumbers=[]
+    rec_pandigital(0, numbers,allNumbers) 
+    return allNumbers
+
+def appendDigit(number, newDigit):
+    return number*10+newDigit
+
+def removeDigit(number, digitToRemove):
+    return int((number-digitToRemove)//10)
+
+
+
 
