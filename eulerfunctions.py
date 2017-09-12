55dc62a85a5deb21d093cc1a2d98a50821b59612
roflmaostc <f.wechsler@tum.de>
2017-09-12 18:52:25 +0200

    added more prime functions

--- eulerfunctions.py
+++ eulerfunctions.py
@@ -22,6 +22,29 @@ def isPrime(number):
     return True
 
 
+
+def prime_factors(n, primes=[], duplicate=True):
+    """This function returns a list of prime factors. Can remove duplicate."""
+    if not primes:
+        primes = generate_primes(int(sqrt(n)//1)+1)
+    factors = []
+    for p in primes:
+        if p*p > n: break 
+        while n%p == 0:
+            factors.append(p)
+            #prevents adding multiple times duplicate
+            if duplicate == True:
+                n //= p
+            else:
+                while n%p == 0:
+                    n //= p
+                break 
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+
 def isPandigital(number,n):
     #slower than is_pandigital
     s = str(number)
@@ -69,8 +92,7 @@ def is_pandigital2(num, digitsA):
 
 
 def rec_pandigital(base, numbers, generatedNumbers):
-    """
-    This function creates all permutations of a number of concatenated elements of parameter numbers
+    """This function creates all permutations of a number of concatenated elements of parameter numbers
     """
     numbersForLoop=numbers.copy()
     for digit in numbersForLoop: 
@@ -111,3 +133,71 @@ def is_triangle_number(num):
 
 def is_hexagonal(h):
     return (1+sqrt(1+8*h)) % 4 == 0
+
+
+def gcd(a,b):
+    """This function calculatest the greatest common divisor (gcd) by using Euklid's algorithm
+    """
+    if b>a:
+        a,b = b,a
+    #assume b<a
+    while b != 0:
+        h = a%b
+        a = b
+        b = h
+    return a
+
+
+def generate_primes(limit):
+    """This functions creates primes up to a limit using sieve of Erastostehens"""
+
+    limit += 1
+    from bitarray import bitarray
+    primes = bitarray(limit)
+    primes.setall(True)
+
+    #0 and 1 are not primes
+    primes[0] = False
+    primes[1] = False
+    
+    #now sieving
+    for i in range(2, len(primes)):
+        counter = 2*i
+        while counter < limit:
+            primes[counter] = False 
+            counter += i
+    
+    return [i for i in range(0,limit) if primes[i]]
+
+
+
+def factor_number(n):
+    """ This algorithm splits a number n into two integers a and b satisfying n = a*b. a and b must not be primes. 
+        The used algorithm is Pollard's rho algorithm.
+        Doesn't guarantee a solution!"""
+   
+
+    if n % 2 == 0 and n != 2:
+        return (2, n//2)
+
+    g = lambda x: (x**2+1) % n
+
+    x = 2
+    y = 2
+    d = 1
+    
+    while d == 1:
+        x = g(x)
+        y = g(g(y))
+        d = gcd(abs(x-y),n)
+    if d == n:
+        #number is prime
+        return n
+    else:
+        #one divisor
+        return (d,n//d)
+
+
+    
+
+
